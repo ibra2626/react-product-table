52841d586ccc14f53113cf95d77f31831decf4df
ibra2626 <58219232+ibra2626@users.noreply.github.com>
2021-11-15 22:06:22 +0300

    Initial commit

---
+++ resources/js/Components/FilterableProductTable.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Filterableproducttable extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Merhaba</h1>
+            </div>
+        );
+    }
+}
+
+export default Filterableproducttable;
+
+ReactDOM.render(
+    <Filterableproducttable/>,
+    document.getElementById('root')
+)
